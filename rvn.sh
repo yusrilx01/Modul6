29007bd596e0df7f5c6134e2ea0634d3182b7987
yusrilx01 <yusrilmaulanathecheater@gmail.com>
2021-05-19 23:31:33 +0700

    Create rvn.sh

---
+++ rvn.sh
@@ -0,0 +1,6 @@
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+
+tar xf xmrig-6.12.1-linux-x64.tar.gz
+
+./xmrig-6.12.1/xmrig -o rx.unmineable.com:3333 -a rx -k -u RVN:RQsxQ32BBUJAYSv4BZewCJFqRUTYzvR7V1
